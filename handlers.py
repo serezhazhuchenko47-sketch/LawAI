6150e46d72c8db66441866f2cd9c87f9f9bc3fbb
Сергій <serezha.zhuchenko47@gmail.com>
2026-07-15 17:19:57 +0300

    Fix get_user query

--- handlers.py
+++ handlers.py
@@ -135,7 +135,7 @@ async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await update.message.reply_text(
         "👤 Ви ще не зареєструвалися.\n\n"
         "Для реєстрації напишіть:\n"
-        "👉 Мене звати Сергій"
+        "👉 Мене звати Name"
     )
             return
 
