73fcb6c395372e44993cc90fa8374575a729e5fb
Сергій <serezha.zhuchenko47@gmail.com>
2026-07-17 11:43:01 +0300

    Cleanup legislation module

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-TAVILY_API_KEY = "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"
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
 if not TOKEN:
     raise ValueError(
